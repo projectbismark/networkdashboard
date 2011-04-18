4f4153763ae5cc9a5c2b60ee4635e249e317c65c
Nick Feamster <feamster@cc.gatech.edu>
2011-04-18 23:53:21 +0000

    bismark read-only user
    git-svn-id: svn+ssh://svn.gtnoise.net/srv/svn/bismark/dashboard@3 f8cb3344-a686-4036-b9e4-0cc1970e7554

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'bismark_live_v1'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'feamster'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_USER = 'bismark_live_ro'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'f8d74cf689'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
