d4c3e5b4f3f839a2f85f706a43e90630789b0163
Nick Feamster <feamster@cc.gatech.edu>
2011-07-19 22:32:29 +0000

    starting postgres branch

--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'bismark_live_v1'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'bismark_live_ro'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'f8d74cf689'         # Not used with sqlite3.
+DATABASE_ENGINE = 'postgresql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'bismark_openwrt_live_v0_1'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'feamster'             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -37,6 +37,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
+# CHANGE to something that is local to you for testing
 MEDIA_ROOT = '/home/feamster/git/networkdashboard/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
