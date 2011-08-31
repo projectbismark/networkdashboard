3908b959aed7281a8359bdc8e874a2f031929ff2
Nick Feamster <feamster@cc.gatech.edu>
2011-08-31 22:05:32 +0000

    sanitize

--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'postgresql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'bismark_openwrt_live_v0_1'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'feamster'             # Not used with sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
@@ -38,7 +38,7 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 # CHANGE to something that is local to you for testing
-MEDIA_ROOT = '/data/users/dashboard/networkdashboard/media/'
+MEDIA_ROOT = '/networkdashboard/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -72,7 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/data/users/dashboard/networkdashboard/templates',
+    '/networkdashboard/templates',
 )
 
 INSTALLED_APPS = (
