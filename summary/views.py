597ac86a4a5b450b32ccb9a04352a0477e5bfe4b
Abhishek Jain <abhishekjain@gatech.edu>
2011-09-21 16:22:44 -0400

    minor bug fix, ignore average of 0

--- summary/views.py
+++ summary/views.py
@@ -275,6 +275,8 @@ def cvs_linegraph(request):
             device_details = MRtt.objects.filter(deviceid=device,eventstamp__gt=start,eventstamp__lte=end,average__lte=chosen_limit, dstip = row_ip["dstip"])
             data1 = list()
             for measure in device_details:
+		if(measure.average <= 0):
+			continue
                 data1.append(str(measure.average) + ", " + str(measure.std))
 
             data.append(data1)
@@ -303,6 +305,8 @@ def cvs_linegraph(request):
         yVariable = request.GET.get('unit')
         output = xVariable + "," + yVariable +"\n"
         for measure in device_details:
+	    if(measure.average <= 0):
+	    	continue
             t = measure.eventstamp
             ret = str(t) + "," + str(measure.average) + ", " + str(measure.std) + "\n"
             output += ret
