1fff8c44e1207e1165b7a063dd54db212b18a325
Abhishek Jain <abhishekjain@gatech.edu>
2011-10-26 12:39:14 -0400

    minor errors

--- summary/views.py
+++ summary/views.py
@@ -129,9 +129,9 @@ def linegraph_bitrate(request):
 	other_device_details_other.extend(all_device_details.filter(deviceid=row.deviceid).exclude(toolid='NETPERF_3'))
 	other_device_details_netperf_3.extend(all_device_details.filter(deviceid=row.deviceid).filter(toolid='NETPERF_3'))	
 	
-    if (graphno==1):
+    if (graphno==2):
 	all_device_details = all_device_details.filter(srcip='143.215.131.173')		
-    elif (graphno==2): 
+    elif (graphno==1): 
         all_device_details = all_device_details.filter(dstip='143.215.131.173')
 
     my_device_details = all_device_details.filter(deviceid=device)
@@ -159,7 +159,7 @@ def linegraph_lmrtt(request):
 
     details = Devicedetails.objects.filter(deviceid=device)[0]
 
-    all_device_details= MBitrate.objects.filter(average__lte=3000,dstip='143.215.131.173').order_by('eventstamp')
+    all_device_details= MLmrtt.objects.filter(average__lte=3000,dstip='143.215.131.173').order_by('eventstamp')
     device_details = all_device_details.filter(deviceid=device)
    
     other_device_details = []
