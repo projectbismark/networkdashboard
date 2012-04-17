7b8fce6d908f824ca5fc3bb27760f3a6e80c814c
Sam Burnett <srburnet@gmail.com>
2012-04-17 04:54:09 +0000

    Fix issues related to tooltips

--- summary/cvs_helper.py
+++ summary/cvs_helper.py
@@ -58,24 +58,13 @@ def linegraph_normal(data,title):
 	return linegraph_normal(data,title,1)
 	
 def linegraph_normal(data,title,factor,roundit):
-    result={}
-    result['name'] = title
-    result['type'] = "line"
-    output= []
-
+    output = []
     for measure in data:
-
-	if(measure.average <= 0):
-		continue
-	temp=[]
-
-	temp.append(datetime_helper.datetime_to_JSON(measure.eventstamp,roundit))
-	temp.append(int(measure.average)*factor)
-	output.append(temp)
-
-    result['data'] = output
-    return result
-
+        if measure.average > 0:
+            output.append(
+                    (datetime_helper.datetime_to_JSON(measure.eventstamp),
+                     float(measure.average) * factor))
+    return dict(name=title, type='line', data=output)
 
 def linegraph_normal_passive(data,title):
     result={}
