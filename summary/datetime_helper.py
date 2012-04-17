bb0b0f937c7c8a08b247dacb6c3ab1883473c898
Sam Burnett <srburnet@gmail.com>
2012-04-17 02:45:31 +0000

    Use UTC instead of local time for datetime conversions

--- summary/datetime_helper.py
+++ summary/datetime_helper.py
@@ -1,13 +1,7 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from datetime import datetime, timedelta
-from time import time,mktime,strftime
+from calendar import timegm
 
 def datetime_to_JSON(time):
-    return int(mktime(time.timetuple()) * 1000)
+    return int(timegm(time.timetuple()) * 1000)
 
 def datetime_format_to_unixtime(time):
-    return mktime(time.timetuple())
-
-def unixtime_to_datetime_format(time):
-    return datetime.fromtimestamp(time)
-
+    return timegm(time.timetuple())
