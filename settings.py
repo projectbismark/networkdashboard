795b5a63f1f85f334c8d0928ac6697072be3ed24
Abhishek Jain <abhishekjain@gatech.edu>
2012-06-27 18:55:39 -0700

    settings changed

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'bismark_openwrt_live_v0_1'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'abhishek'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'dashboard'         # Not used with sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
