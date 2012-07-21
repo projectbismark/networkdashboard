d11d9c9d7e28e3d3504a78000942ce77ee8ff227
aroberts3@gatech.edu <Alfred@.(none)>
2012-07-21 10:40:22 -0400

    new file

---
+++ summary/isp_mappings.py
@@ -0,0 +1,8 @@
+mappings = [
+	['comcast','Comcast'],
+	['AT&T','AT&T'],
+	['bellsouth', 'AT&T'],
+	['airtel', 'Airtel'],
+	['mweb', 'MWEB'],
+	['university of kentucky','University of Kentucky']
+]
