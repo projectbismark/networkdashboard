3704a30fa12f80c5cb97d3d3a7da94e39eb5b2df
aroberts3@gatech.edu <Alfred@.(none)>
2013-03-13 09:41:39 -0400

    minor style changes, add ability to hide descriptions

--- media/device.js
+++ media/device.js
@@ -10,20 +10,53 @@ function expandUploadDesc() {
     $('#upload_desc').hide();
     return false;
 }
+
+function collapseUploadDesc() {
+    $('#upload_desc_content').hide();
+    $('#upload_desc').show();
+    return false;
+}
 function expandRttDesc() {
     $('#rtt_desc_content').show();
     $('#rtt_desc').hide();
     return false;
 }
+
+function collapseRttDesc() {
+    $('#rtt_desc_content').hide();
+    $('#rtt_desc').show();
+    return false;
+}
+
 function expandLmrttDesc() {
     $('#lmrtt_desc_content').show();
     $('#lmrtt_desc').hide();
     return false;
 }
+
+function collapseLmrttDesc() {
+    $('#lmrtt_desc_content').hide();
+    $('#lmrtt_desc').show();
+    return false;
+}
+
 function expandDownloadDesc() {
     $('#download_desc_content').show();
     $('#download_desc').hide();
     return false;
+}
+
+function collapseDownloadDesc() {
+    $('#download_desc_content').hide();
+    $('#download_desc').show();
+    return false;
+}
+
+function collapseDesc() {
+	collapseUploadDesc();
+	collapseDownloadDesc();
+	collapseRttDesc();
+	collapseLmrttDesc();
 }
 
 function setTabid(data){
