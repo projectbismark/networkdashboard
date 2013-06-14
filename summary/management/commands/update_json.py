5def1eb9ea0a744529784e2052dc91fd711ae4ac
aroberts3@gatech.edu <Alfred@.(none)>
2013-06-14 05:28:37 -0400

    added lock to update method

--- summary/management/commands/update_json.py
+++ summary/management/commands/update_json.py
@@ -1,6 +1,9 @@
 from django.core.management.base import NoArgsCommand
 from networkdashboard.summary import database_helper
 from networkdashboard.summary.models import *
+import threading
+
+lock = threading.Lock()
 
 class Command(NoArgsCommand):
 	def handle_noargs(self, **options):
@@ -8,15 +11,21 @@ class Command(NoArgsCommand):
 	
 
 def update():
-	all_devices = Devicedetails.objects.all().values('deviceid')
-	device_list = []
-	for device in all_devices:
-		device_list.append(device['deviceid'])
-	for device in device_list:
-		database_helper.update_bitrate(device)
-		database_helper.update_rtt(device)
-		database_helper.update_lmrtt(device)
-		database_helper.update_shaperate(device)
-		database_helper.update_capacity(device)
-		database_helper.update_unload(device)
+	t = threading.Thread(target = update_json, args = ())
+	t.start()
+	
+def update_json():
+	if(lock.acquire(False)):
+		all_devices = Devicedetails.objects.all().values('deviceid')
+		device_list = []
+		for device in all_devices:
+			device_list.append(device['deviceid'])
+		for device in device_list:
+			database_helper.update_bitrate(device)
+			database_helper.update_rtt(device)
+			database_helper.update_lmrtt(device)
+			database_helper.update_shaperate(device)
+			database_helper.update_capacity(device)
+			database_helper.update_unload(device)
+		lock.release()
 	return
