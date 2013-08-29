19ba029a72f8723e2ebfa1bfcf20f9bbb8190f33
aroberts3@gatech.edu <Alfred@.(none)>
2013-08-29 16:10:18 -0400

    minor

--- summary/management/commands/update_json.py
+++ summary/management/commands/update_json.py
@@ -38,23 +38,23 @@ class Command(NoArgsCommand):
 def update_json():
 	# lock = UpdateLock('/tmp/UpdateLock.tmp')
 	# if (lock.acquire()):
-	all_devices = Devicedetails.objects.all().values('deviceid')
-	count = 0
-	t0 = datetime.now()
-	for device in all_devices:
-		device = device['deviceid']
-		database_helper.update_bitrate(device)
+	# all_devices = Devicedetails.objects.all().values('deviceid')
+	#count = 0
+	#t0 = datetime.now()
+	#for device in all_devices:
+		#device = device['deviceid']
+		#database_helper.update_bitrate(device)
 		# database_helper.update_rtt(device)
-		database_helper.update_lmrtt(device)
-		database_helper.update_shaperate(device)
-		database_helper.update_capacity(device)
-		database_helper.update_unload(device)
+		#database_helper.update_lmrtt(device)
+		#database_helper.update_shaperate(device)
+		#database_helper.update_capacity(device)
+		#database_helper.update_unload(device)
 		#lock.release()
-		count+=1
-		t1 = datetime.now()
-		print t1-t0
-		print str(len(all_devices)-count) + " remaining"
-	write_rtt_measurements()
+		#count+=1
+		#t1 = datetime.now()
+		#print t1-t0
+		#print str(len(all_devices)-count) + " remaining"
+	#write_rtt_measurements()
 	write_rtt_averages()
 	return
 
@@ -113,9 +113,9 @@ def write_rtt_averages():
 		records = cursor.fetchall()
 		for r in records:
 			isp = r['isp']
-			avg = r['avg']
-			count = r['count']
-			line = isp ',' + avg + ',' + count + '\n'
+			avg = str(r['avg'])
+			count = str(r['count'])
+			line = isp + ',' + avg + ',' + count + '\n'
 			f.write(line)
 		f.close()
 	for c in countries:
@@ -136,9 +136,9 @@ def write_rtt_averages():
 		records = cursor.fetchall()
 		for r in records:
 			isp = r['isp']
-			avg = r['avg']
-			count = r['count']
-			line = isp ',' + avg + ',' + count + '\n'
+			avg = str(r['avg'])
+			count = str(r['count'])
+			line = isp + ',' + avg + ',' + count + '\n'
 			f.write(line)
 		f.close()
 	for isp in isps:
@@ -159,9 +159,9 @@ def write_rtt_averages():
 		records = cursor.fetchall()
 		for r in records:
 			city = r['city']
-			avg = r['avg']
-			count = r['count']
-			line = city ',' + avg + ',' + count + '\n'
+			avg = str(r['avg'])
+			count = str(r['count'])
+			line = city + ',' + avg + ',' + count + '\n'
 			f.write(line)
 		f.close()
 	cursor.close()
