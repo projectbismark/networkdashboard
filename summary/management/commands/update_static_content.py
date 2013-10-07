ddb887cfecdec2be343d739b9ca64ffcdc834d8d
aroberts3@gatech.edu <Alfred@.(none)>
2013-10-07 11:32:27 -0400

    minor

--- summary/management/commands/update_static_content.py
+++ summary/management/commands/update_static_content.py
@@ -106,7 +106,7 @@ def write_devices():
 		isp = rec['geoip_isp']
 		if isp==None:
 			isp='none'
-		line = id + '|' + str(eventstamp) + '|' + city.encode('utf-8') + '|' + country.encode('utf-8') + '|' + isp.encode('utf-8') + '|' + str(recent) + '\n'
+		line = id + '|' + str(eventstamp) + '|' + city.encode('utf-8') + '|' + country.encode('utf-8') + '|' + isp.encode('utf-8') + '|' + str(active) + '\n'
 		file.write(line)
 	file.close()
 	cursor.close()
