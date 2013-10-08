af4b3a007bf0d6917b014363b91a7a3fe728375b
aroberts3@gatech.edu <Alfred@.(none)>
2013-10-08 14:23:28 -0400

    minor

--- media/rendergraphs.js
+++ media/rendergraphs.js
@@ -999,7 +999,7 @@ function onSuccessIspCompareBar(graphParams,country) {
 							units = "ms";
 							val = this.y;
 						}
-						return title + "<br/>" + val.toFixed(2) + units + "<br/> (Click for more information)";
+						return title + "<br/>" + val.toFixed(2) + units;
 					}
 				},
 				legend:{
